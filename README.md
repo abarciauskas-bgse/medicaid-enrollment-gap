7196c554750494860568ee84d84e5ac578b7ff56
Aimee Barciauskas <aimee@navahq.com>
2017-10-18 22:10:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Data
+
+* Poverty data from census.gov
+  * [page](https://www.census.gov/data/tables/2017/demo/income-poverty/p60-259.html)
+  * [xls file](https://www2.census.gov/programs-surveys/demo/tables/p60/259/statepov.xls)
+* Population data from census.gov
+  * [page](https://www2.census.gov/programs-surveys/popest/datasets/2010-2016/state/asrh/)
+  * [csv file](https://www2.census.gov/programs-surveys/popest/datasets/2010-2016/state/asrh/scprc-est2016-18+pop-res.csv)
+* Medicaid enrollment from medicaid.gov
+  * [page](https://www.medicaid.gov/medicaid/program-information/medicaid-and-chip-enrollment-data/enrollment-mbes/index.html)
+  * [pdf file](https://www.medicaid.gov/medicaid/program-information/downloads/cms-64-enrollment-report-jul-aug-2016.pdf) - copy / pasted data to create CSV
+  * data not available for South Dakota
---
+++ data/create-gap.py
@@ -0,0 +1,58 @@
+import csv
+import pandas as pd
+
+population_metadata = {
+ 'file': 'data/scprc-est2016-18+pop-res.csv',
+ 'state_col': 'NAME',
+ 'data_col': 'POPESTIMATE2016'
+}
+
+medicaid_metadata = {
+  'file': 'data/cms-64-enrollment-report-jul-aug-2016.csv',
+  'state_col': 'State',
+  'data_col': 'Total Medicaid Enrollees'
+}
+
+poverty_metadata = {
+  'file': 'data/perct-poverty.csv',
+  'state_col': 'State',
+  'data_col': '3yr-average-2014-2016'
+}
+
+# TODO(aimee): Create function for all this
+with open(population_metadata['file']) as f:
+  population = pd.DataFrame([{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)])
+  data_col = population_metadata['data_col']
+  population[data_col] = [int(value.replace(',','')) for value in population[data_col]]
+
+with open(medicaid_metadata['file']) as f:
+  medicaid_enrollment = pd.DataFrame([{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)])
+  data_col = medicaid_metadata['data_col']
+  medicaid_enrollment[data_col] = [int(value.replace(',','')) for value in medicaid_enrollment[data_col]]
+
+with open(poverty_metadata['file']) as f:
+  poverty_rates = pd.DataFrame([{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)])
+  data_col = poverty_metadata['data_col']
+  poverty_rates[data_col] = [float(value.replace(',','')) for value in poverty_rates[data_col]]  
+
+merged_data = population.merge(
+  medicaid_enrollment,
+  left_on=population_metadata['state_col'],
+  right_on=medicaid_metadata['state_col'])
+
+merged_data = merged_data.merge(
+  poverty_rates,
+  left_on=population_metadata['state_col'],
+  right_on=poverty_metadata['state_col'])
+
+# Medicaid expansion covers all those earning 133% of the poverty level, but
+# there's no good way to factor this in w/o knowing the distribution of income
+# levels.
+# https://www.healthcare.gov/medicaid-chip/medicaid-expansion-and-you/
+merged_data['medicaid_rate'] = (merged_data[medicaid_metadata['data_col']] / merged_data[population_metadata['data_col']]) * 100
+merged_data['gap'] = merged_data['medicaid_rate'] - merged_data['3yr-average-2014-2016']
+
+print(merged_data[['NAME','medicaid_rate','3yr-average-2014-2016','gap']])
+pd.DataFrame.to_csv(merged_data[['NAME','medicaid_rate','3yr-average-2014-2016','gap']], path_or_buf='medicaid-poverty-gap.csv')
+
+
